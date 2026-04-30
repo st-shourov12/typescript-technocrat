4da01f5dbcb16416e1363aed4bd783d153483fa9
st-shourov12 <shourovsc38@gmail.com>
2026-04-30 23:47:25 +0600

    exploring object

--- src/non-premitive.ts
+++ src/non-premitive.ts
@@ -10,3 +10,23 @@ console.log(mixedArr);
 // tuple
 let coordinate: [number, number, number] = [20,30,25];
 let destination:[string, string, string] = ['Dhaka', 'Chattagram', '3hrs']
+
+
+
+// reference type : object 
+const user : {
+    x : "x" ; //fixed value (literal type)
+    readonly isMarried : boolean; //access modifier
+    firstName :string ;
+    middleName?: string; //optional
+    lastName: string
+} = {
+    x : 'x',
+    isMarried : false,
+    firstName : 'Mirazul',
+    // middleName : 'Islam',
+    lastName : 'Shourov'
+
+}
+
+console.log(user?.lastName);
