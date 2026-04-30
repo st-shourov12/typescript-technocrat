cbc693f5d1b9e6a58938cda8266204aa4db26d85
st-shourov12 <shourovsc38@gmail.com>
2026-04-30 21:48:42 +0600

    exploring premitive data types

---
+++ dist/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "Next Level Development";
+console.log(course);
+//# sourceMappingURL=test.js.map
---
+++ src/premitive.ts
@@ -0,0 +1,9 @@
+// JS - TS
+// string , number , boolean , undefined , null
+
+// TS: never, unknown , void
+
+let userName : string = "Shourov"
+let userID = 123 ;
+
+console.log("The user name is ", userName , " user ID is ", userID);
