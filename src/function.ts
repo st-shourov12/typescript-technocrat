fbb0a1afff433f5a1b83c9e5a98729816da3056e
st-shourov12 <shourovsc38@gmail.com>
2026-05-01 00:24:31 +0600

    exploring arrow function

--- src/function.ts
+++ src/function.ts
@@ -1,12 +1,29 @@
 //function
-//arrow function , normal function
 
+
+//arrow function , normal function
 function add (num1 : number, num2 : number): number{
     return num1 + num2 ;
 
 }
-
 const sum = add(2,2);
 console.log(sum);
 
 
+// arrow function
+const sub = (num1: number, num2: number):number => num1 - num2 ;
+console.log(sub(12,5));
+
+
+//object => function => method
+
+const poorUser = {
+    name : 'Shourov',
+    balance : 0 ,
+    addBalance (newBalance : number) : number {
+        const totalBalance = this.balance + newBalance ;
+       return totalBalance
+    }
+}
+
+console.log(poorUser.addBalance(2500))
