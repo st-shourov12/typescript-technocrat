ad97742de0846109e3fdf93fcf2e789423d171fd
st-shourov12 <shourovsc38@gmail.com>
2026-05-01 00:10:07 +0600

    exploring normal function

---
+++ src/function.ts
@@ -0,0 +1,12 @@
+//function
+//arrow function , normal function
+
+function add (num1 : number, num2 : number): number{
+    return num1 + num2 ;
+
+}
+
+const sum = add(2,2);
+console.log(sum);
+
+
