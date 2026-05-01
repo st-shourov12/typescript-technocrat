373dcb719c2e0514cfc0718dd6dbf3b0df74effc
st-shourov12 <shourovsc38@gmail.com>
2026-05-01 15:31:42 +0600

    learning rest operator

--- src/spreadandrest.ts
+++ src/spreadandrest.ts
@@ -21,4 +21,19 @@ const otherInfo: {isMarried : boolean ; jobStatus : string} = {
 }
 
 const userInfo = {...user , ...otherInfo};
-console.log(userInfo);
+console.log(`User information ${userInfo}`);
+
+// rest operator
+// const sendInvite = (friend1 : string , friend2: string, friend3 : string)  => {
+//     console.log(`Welcome ${friend1}`);
+//     console.log(`Welcome ${friend2}`);
+//     console.log(`Welcome ${friend3}`);
+// };
+
+// sendInvite('Mintu', 'Pintu', 'Chintu');
+
+const sendInvite = (...guys : string[]) => {
+    guys.forEach((guy:string) => console.log(`Send Invitation to ${guy}`));
+}
+
+sendInvite('Gulti', 'Multi', 'Sokina')
